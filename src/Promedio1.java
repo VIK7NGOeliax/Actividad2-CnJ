b6fefb2d82172d4d52f180761da8e4a9f91f267d
Elias <eliax.snm@gmail.com>
2023-06-06 15:23:42 -0600

    Creación del programa de Promedio Sesion de Asesoria

---
+++ src/Promedio1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Promedio1 {
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        double calificacion1,calificacion2,calificacion3,calificacion4,calificacion5,promedio;
+        String alumno;
+
+        System.out.println("Nombre del estudiante: ");
+        alumno=teclado.nextLine();
+        System.out.println("Calificacion 1: ");
+        calificacion1=teclado.nextDouble();
+        System.out.println("Calificacion 2: ");
+        calificacion2=teclado.nextDouble();
+        System.out.println("Calificacion 3: ");
+        calificacion3=teclado.nextDouble();
+        System.out.println("Calificacion 4: ");
+        calificacion4=teclado.nextDouble();
+        System.out.println("Calificacion 5: ");
+        calificacion5=teclado.nextDouble();
+        promedio=(calificacion1+calificacion2+calificacion3+calificacion4+calificacion5)/5;
+        System.out.println("El promedio final del estudiante: "+alumno+" es de: "+promedio);
+
+
+    }
+}
